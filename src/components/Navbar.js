f4e68ef67501304bf3e6e436046bf391cd675685
Mortron <cmsl4y3r@gmail.com>
2021-11-13 19:43:36 -0700

    finally finished.  Needed children in wrapper

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,12 @@ import React, {useContext} from 'react';
 import NameContext from '../context/NameContext';
 
 const Navbar = (props) => {
+    // Using the context I created.  Imported above
     const {name} = useContext(NameContext);
 
     return(
         <div className="navBar">
-            <h1 className="navHeader">Go {name} &nbsp;</h1>;
+            <h1 className="navHeader">Go {name}! &nbsp;</h1>;
         </div>
     )
 }
